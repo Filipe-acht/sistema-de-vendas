848879b9715d533118c720933ced028d5b1a65c1
filipe.soares <filipebnks@gmail.com>
2024-01-04 17:44:11 -0300

    feat: client API created
    updating project with all codes

--- clients/src/main/java/io/github/filipeacht/clients/ClientApplication.java
+++ clients/src/main/java/io/github/filipeacht/clients/ClientApplication.java
@@ -1,23 +1,18 @@
 package io.github.filipeacht.clients;
 
-import io.github.filipeacht.clients.model.entity.Client;
-import io.github.filipeacht.clients.model.repository.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientApplication {
 
-    @Bean
+    /*@Bean
     public CommandLineRunner run(@Autowired ClientRepository repository){
         return args ->{
             Client client = Client.builder().cpf("01234567891").name("Stahlmann").build();
             repository.save(client);
         };
-    }
+    }*/
 
     public static void main(String[] args) {
 
--- clients/src/main/java/io/github/filipeacht/clients/model/entity/Client.java
+++ clients/src/main/java/io/github/filipeacht/clients/model/entity/Client.java
@@ -1,8 +1,12 @@
 package io.github.filipeacht.clients.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -17,12 +21,16 @@ public class Client {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty(message = "{field.name.required}")
     private String name;
 
     @Column(nullable = false, length = 11)
+    @NotNull(message = "{field.cpf.required}")
+    @CPF(message = "{field.cpf.invalid}")
     private String cpf;
 
-    @Column(name = "register_date")
+    @Column(name = "register_date", updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate registerDate;
 
     @PrePersist
