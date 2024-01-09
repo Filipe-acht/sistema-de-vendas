cba20b302f9b5655738be13de5b206285834102f
filipe.soares <filipebnks@gmail.com>
2024-01-09 15:53:06 -0300

    feat: updating clients-form

--- front end/clients-app/src/app/clients/clients-form/clients-form.component.ts
+++ front end/clients-app/src/app/clients/clients-form/clients-form.component.ts
@@ -11,17 +11,26 @@ import { ClientsService } from 'src/app/clients.service';
 export class ClientsFormComponent implements OnInit {
 
   client: Client;
+  success: boolean = false;
+  errors: String[];
 
-  constructor( private service: ClientsService) {
-    //this.client = new Client();
-    this.client = service.getClient();
-
+  constructor(private service: ClientsService) {
+    this.client = new Client();
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit(){
-    console.log(this.client);
+  onSubmit() {
+    this.service.save(this.client)
+      .subscribe(response => {
+        this.success = true;
+        this.errors = null;
+        this.client = response;
+      }, errorResponse => {
+        this.success = false;
+        this.errors = errorResponse.error.errors;
+      }
+      )
   }
 }
